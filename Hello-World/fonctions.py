6722a7954d3c2a5a8b0541a6af797881131bd7ab
Hélène DALON-DENEE <pipinetamusique@gmail.com>
2024-11-28 11:45:06 +0100

    Add soustraction function and print my name in fonctions.py

--- Hello-World/fonctions.py
+++ Hello-World/fonctions.py
@@ -6,7 +6,7 @@ def addition(x, y):
 
 def soustraction(x, y):
     """Retourne la différence de x et y"""
-    return 
+    return x-y
 
 
 def noms_binome():
@@ -14,6 +14,7 @@ def noms_binome():
     Affiche les noms des membres du binôme
     Attention, chacun écrit la ligne pour afficher son nom
     """
+    print("Hélène DALON-DENEE")
 
 
 a = 2
